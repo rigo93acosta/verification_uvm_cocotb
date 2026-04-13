258dde8f1933833620328aafeae554656b0dbaba
rigo93acosta <rigo93acosta@gmail.com>
2026-04-13 09:45:38 -0300

    style: Improve formatting and alignment in I2C module code

--- Course_4/5_I2C/i2c.sv
+++ Course_4/5_I2C/i2c.sv
@@ -2,46 +2,46 @@
 `include "i2c_m.sv"
 
 module i2c (
-    input            clk,
-    input            rst,
-    input           newd,
-    input             op,
-    input  [6:0]    addr,
-    input  [7:0]     din,
-    output [7:0]    dout,
-    output          busy,
+    input        clk,
+    input        rst,
+    input        newd,
+    input        op,
+    input  [6:0] addr,
+    input  [7:0] din,
+    output [7:0] dout,
+    output       busy,
     output       ack_err,
-    output          done
-  );
+    output       done
+);
 
   wire sda, scl;
   wire ack_errm, ack_errs;
 
   // Simular resistencias pull-up de 4.7kΩ del bus I2C
-  pullup(sda);
-  pullup(scl);
+  pullup (sda);
+  pullup (scl);
 
   i2c_m master (
-    .clk     (clk),
-    .rst     (rst),
-    .newd    (newd),
-    .addr    (addr),
-    .op      (op),
-    .sda     (sda),
-    .scl     (scl),
-    .din     (din),
-    .dout    (dout),
-    .busy    (busy),
-    .ack_err (ack_errm),
-    .done    (done)
+      .clk    (clk),
+      .rst    (rst),
+      .newd   (newd),
+      .addr   (addr),
+      .op     (op),
+      .sda    (sda),
+      .scl    (scl),
+      .din    (din),
+      .dout   (dout),
+      .busy   (busy),
+      .ack_err(ack_errm),
+      .done   (done)
   );
 
   i2c_s slave (
-    .scl     (scl),
-    .clk     (clk),
-    .rst     (rst),
-    .sda     (sda),
-    .ack_err (ack_errs)
+      .scl    (scl),
+      .clk    (clk),
+      .rst    (rst),
+      .sda    (sda),
+      .ack_err(ack_errs)
   );
 
   assign ack_err = ack_errm | ack_errs;
