81f444c049c546428a39835af91f7aad0d879b7b
rigo93acosta <rigo93acosta@gmail.com>
2026-05-20 19:49:49 -0300

    fix: Some errors and update Makefile

--- Course_2/1_Intro/2_reset/adder_tb.py
+++ Course_2/1_Intro/2_reset/adder_tb.py
@@ -1,31 +1,30 @@
 import cocotb
-import logging
 from cocotb.triggers import Timer, RisingEdge, FallingEdge, Edge
 
 
 @cocotb.test()
 async def test(dut):
 
-    ## fixed duration
-    # rst = dut.rst
-    # rst.value = 1
-    # await Timer(50, unit='ns')
-    # rst.value = 0
-    # await Timer(100, unit='ns')
-
-    ## Ver variables and methods of the DUT
-    # print(dir(dut))
-
-    ## fixed duration rst - 2 pos edges clk
+    # fixed duration
     rst = dut.rst
     rst.value = 1
-    dut._log.info("Resetting DUT")
-    await Edge(dut.clk)
-    await Edge(dut.clk)
+    await Timer(50, unit="ns")
     rst.value = 0
-    dut._log.info("Releasing DUT from reset")
-    await RisingEdge(dut.clk)
-    await RisingEdge(dut.clk)
-    dut._log.info("Finished reset sequence")
-    await Timer(100, unit='ns')
-    dut._log.info("Test complete")
+    await Timer(100, unit="ns")
+
+    # Ver variables and methods of the DUT
+    # print(dir(dut))
+
+    # fixed duration rst - 2 pos edges clk
+    # dut.rst.value = 1
+    # dut._log.info("Resetting DUT")
+    # await Edge(dut.clk)
+    # await Edge(dut.clk)
+    # dut.rst.value = 0
+    # dut._log.info("Releasing DUT from reset")
+    # await RisingEdge(dut.clk)
+    # await RisingEdge(dut.clk)
+    # dut._log.info("Finished reset sequence")
+    # await Timer(100, unit="ns")
+    # dut._log.info("Test complete")
+
