c51af4742840ef88284a70fb5abc30ea028d75f4
rigo93acosta <rigo93acosta@gmail.com>
2026-05-20 20:01:36 -0300

    refactor: new detect value signal change

--- Course_2/1_Intro/2_reset/adder_tb.py
+++ Course_2/1_Intro/2_reset/adder_tb.py
@@ -1,30 +1,30 @@
 import cocotb
-from cocotb.triggers import Timer, RisingEdge, FallingEdge, Edge
+from cocotb.triggers import Timer
 
 
 @cocotb.test()
 async def test(dut):
 
     # fixed duration
-    rst = dut.rst
-    rst.value = 1
-    await Timer(50, unit="ns")
-    rst.value = 0
-    await Timer(100, unit="ns")
+    # rst = dut.rst
+    # rst.value = 1
+    # await Timer(50, unit="ns")
+    # rst.value = 0
+    # await Timer(100, unit="ns")
 
     # Ver variables and methods of the DUT
     # print(dir(dut))
 
     # fixed duration rst - 2 pos edges clk
-    # dut.rst.value = 1
-    # dut._log.info("Resetting DUT")
-    # await Edge(dut.clk)
-    # await Edge(dut.clk)
-    # dut.rst.value = 0
-    # dut._log.info("Releasing DUT from reset")
-    # await RisingEdge(dut.clk)
-    # await RisingEdge(dut.clk)
-    # dut._log.info("Finished reset sequence")
-    # await Timer(100, unit="ns")
-    # dut._log.info("Test complete")
-
+    dut.rst.value = 1
+    dut._log.info("Resetting DUT")
+    await dut.clk.value_change
+    await dut.clk.value_change
+    # await Edge(dut.clk) # Deprecated
+    dut.rst.value = 0
+    dut._log.info("Releasing DUT from reset")
+    await dut.clk.rising_edge
+    await dut.clk.rising_edge
+    dut._log.info("Finished reset sequence")
+    await Timer(100, unit="ns")
+    dut._log.info("Test complete")
