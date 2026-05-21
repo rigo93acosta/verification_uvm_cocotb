18af152a5f27411c7d6a470e286f080ab11d56ce
rigo93acosta <rigo93acosta@gmail.com>
2026-05-21 13:04:56 -0300

    refactor: replace logging with dut._log for test output

--- Course_2/2_Combinational/4bit_adder/adder_tb.py
+++ Course_2/2_Combinational/4bit_adder/adder_tb.py
@@ -1,31 +1,32 @@
 import cocotb
-import logging
 import random
 from cocotb.triggers import Timer
 from cocotb.utils import get_sim_time
 
+
 @cocotb.test()
 async def test(dut):
-      logging.getLogger().setLevel(logging.INFO)
-      err = 0
-      for i in range(10):
-          a = random.randint(0,15)
-          b = random.randint(0,15)
-          dut.a.value = a
-          dut.b.value = b
-          await Timer(10,unit = 'ns')
-          y = dut.y.value.to_unsigned()
-          
-          if(y == (a + b)):
-            logging.info('Test Passed: a:%0d, b:%0d and y:%0d @ %0s',
-                         a,b,y,str(get_sim_time(unit='ns')))
-          else:
-            logging.error('Test Failed: a:%0d, b:%0d and y:%0d @ %0s',
-                          a,b,y,str(get_sim_time(unit='ns')))
+    err = 0
+    for i in range(10):
+        a = random.randint(0, 15)
+        b = random.randint(0, 15)
+        dut.a.value = a
+        dut.b.value = b
+        await Timer(10, unit="ns")
+        y = dut.y.value.to_unsigned()
+
+        if y == (a + b):
+            dut._log.info(
+                f"Test Passed: a:{a:02d}, b:{b:02d} and y:{y:02d} @ {get_sim_time(unit='ns')}"
+            )
+        else:
+            dut._log.error(
+                f"Test Passed: a:{a:02d}, b:{b:02d} and y:{y:02d} @ {get_sim_time(unit='ns')}"
+            )
             err = err + 1
-          print('---------------------------------------------------------')  
-          
-      if(err > 0):
-          logging.error('Number of failed test cases : %0d',err)
-      else:
-          logging.info('All the test cases Passed')
+        dut._log.info("---------------------------------------------------------")
+
+    if err > 0:
+        dut._log.error(f"Number of failed test cases : {err:02d}")
+    else:
+        dut._log.info("All the test cases Passed")
