053b7d34d969114e5d6f9b5101e1552e7f82de02
rigo93acosta <rigo93acosta@gmail.com>
2026-05-21 21:48:09 -0300

    refactor: remove makefile and update memory module and testbench for improved logging

--- Course_2/3_Sequential/memory/memory.sv
+++ Course_2/3_Sequential/memory/memory.sv
@@ -1,42 +1,26 @@
-module top(
-    input rst,clk,
+module memory (
+    input rst,
+    input clk,
     input wr,
     input [7:0] din,
     input [3:0] addr,
     output [7:0] dout
-  );
+);
 
-  reg [7:0] mem [16];
+  reg [7:0] mem[16];
   reg [7:0] temp = 8'h00;
 
-  always@(posedge clk)
-  begin
-    if     (rst == 1'b1)
-    begin
-      for (int i =0; i < 16; i++)
-      begin
+  always @(posedge clk) begin
+    if (rst == 1'b1) begin
+      for (int i = 0; i < 16; i++) begin
         mem[i] <= 0;
       end
-      temp      <= 8'h0;
-    end
-    else if (wr == 1'b1)
-    begin
+      temp <= 8'h0;
+    end else if (wr == 1'b1) begin
       mem[addr] <= din;
-    end
-    else
-      temp      <= mem[addr];
+    end else temp <= mem[addr];
   end
-
 
   assign dout = temp;
-
-
-
-  initial
-  begin
-    $dumpfile("memory.vcd");
-    $dumpvars(1,top);
-  end
-
 
 endmodule
--- Course_2/3_Sequential/memory/memory_tb.py
+++ Course_2/3_Sequential/memory/memory_tb.py
@@ -1,78 +1,89 @@
 import cocotb
-import logging
 import random
 
-from cocotb.triggers import Timer, FallingEdge, ClockCycles, RisingEdge
+from cocotb.triggers import Timer, ClockCycles, Combine
 from cocotb.utils import get_sim_time
 from cocotb.clock import Clock
 
 mem_arr = {}
 
+
 async def rst_stimuli(dut):
-    logging.info('Applying reset to DUT @ %0s', str(get_sim_time('ns')))
+    dut._log.info(f"Applying reset to DUT @ {get_sim_time('ns')}")
     for i in range(16):
-        mem_arr.update({i:0})    
+        mem_arr.update({i: 0})
     dut.rst.value = 1
-    await Timer(100, 'ns')
+    await Timer(100, "ns")
     dut.rst.value = 0
-    logging.info('System Reset Done @ %0s', str(get_sim_time('ns')))
-    print(mem_arr)
-    
+    dut._log.info(f"System Reset Done @ {get_sim_time('ns')}")
+    dut._log.info(f"Memoria {mem_arr}")
+
 
 async def clk_stimuli(dut):
     while True:
         dut.clk.value = 1
-        await Timer(10, 'ns')
+        await Timer(10, "ns")
         dut.clk.value = 0
-        await Timer(10, 'ns')
+        await Timer(10, "ns")
+
 
 async def write_data(dut):
-    await FallingEdge(dut.rst)
-    logging.info('--------------------------------------------------')
-    logging.info('Writing Data to Memory')
-    logging.info('--------------------------------------------------')
+    await dut.rst.falling_edge
+    dut._log.info("--------------------------------------------------")
+    dut._log.info("Writing Data to Memory")
+    dut._log.info("--------------------------------------------------")
     for i in range(15):
         din = random.randint(0, 255)
         addr = random.randint(0, 15)
-        mem_arr.update({addr:din})
+        mem_arr.update({addr: din})
         dut.din.value = din
         dut.addr.value = addr
         dut.wr.value = 1
-        logging.info('---------addr: %0d,Din : %0d @ %0s---------',addr,din, str(get_sim_time('ns')))
+        dut._log.info(
+            f"--------- addr: {addr:02d}, "
+            f"Din :  {din:03d} @ "
+            f"{get_sim_time('ns'):>04.0f} ---------"
+        )
         await ClockCycles(dut.clk, 2)
     dut.wr.value = 0
-    print(mem_arr) 
     
+    dut._log.info(f"Memoria {mem_arr}")
+
 
 async def read_data(dut):
     err = 0
-    await FallingEdge(dut.wr)
-    logging.info('--------------------------------------------------')
-    logging.info('Reading Data from Memory')
-    logging.info('--------------------------------------------------')
+    await dut.wr.falling_edge
+    dut._log.info("--------------------------------------------------")
+    dut._log.info("Reading Data from Memory")
+    dut._log.info("--------------------------------------------------")
     for i in range(15):
         addr = random.randint(0, 15)
         dut.addr.value = addr
         dut.wr.value = 0
         await ClockCycles(dut.clk, 2)
         dout = dut.dout.value
-        logging.info('---------addr: %0d,Dout : %0d @ %0s---------',addr,dout, str(get_sim_time('ns')))
-        if mem_arr.get(addr) != dout  :
-           err += 1
-        else :
-           err = err 
+        dut._log.info(
+            f"--------- addr: {addr:02d}, "
+            f"Dout : {dout.to_unsigned():03d} @ "
+            f"{get_sim_time('ns'):>04.0f} ---------"
+        )
+        if mem_arr.get(addr) != dout:
+            err += 1
+        else:
+            err = err
     if err > 0:
-         logging.error('Test Failed for %0d test cases',err)
+        dut._log.error(f"Test Failed for {err} test cases")
     else:
-         logging.info('All Test Cases Passed')        
-        
+        dut._log.info("All Test Cases Passed")
+
 
 @cocotb.test()
 async def test(dut):
-    logging.getLogger().setLevel(logging.INFO)
 
-    cocotb.start_soon(rst_stimuli(dut))
-    cocotb.start_soon(clk_stimuli(dut))
-    cocotb.start_soon(write_data(dut))
-    cocotb.start_soon(read_data(dut))
-    await Timer(2000,'ns')
+    reset = cocotb.start_soon(rst_stimuli(dut))
+    clock = cocotb.start_soon(clk_stimuli(dut))
+    write = cocotb.start_soon(write_data(dut))
+    read = cocotb.start_soon(read_data(dut))
+    await Combine(reset, write, read)
+    clock.cancel()
+    dut._log.info("Test Completed")
