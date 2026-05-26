99e5022318f88af144d307cf39f53c5c22702cc5
rigo93acosta <rigo93acosta@gmail.com>
2026-05-26 12:01:48 -0300

    feat: refactor dff testbench and add runner script for improved simulation management

--- Course_4/2_DFF/dff_tb.py
+++ Course_4/2_DFF/dff_tb.py
@@ -1,8 +1,7 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Event, Timer, ClockCycles
+from cocotb.triggers import Event, ClockCycles, ReadOnly
 from cocotb_coverage.crv import Randomized
-from cocotb.utils import get_sim_time
 from cocotb.queue import Queue
 import random
 
@@ -26,13 +25,17 @@ class Generator():
         self.count = count
         self.event.clear()
 
-    async def gen_data(self):
+    async def gen_data(self, seq_random=True):
 
+        seq = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
         for i in range(self.count):
             t = Transaction()
-            t.randomize()
+            if not seq_random:
+                t.din = int(seq[i])
+            else:
+                t.randomize()
 
-            cocotb.log.info(f"[GEN]: din: {t.din} @ : {get_sim_time(unit = 'ns')}")
+            cocotb.log.info(f"[GEN]: din: {t.din:02d}")
             await self.queue.put(t)
             await self.event.wait()
             self.event.clear()
@@ -44,23 +47,23 @@ class Driver():
         self.dut = dut
         
     async def reset_dut(self):
+        await self.dut.clk.falling_edge
         self.dut.rst.value = 1
         self.dut.din.value = 0
-        cocotb.log.info(f"--------Reset Applied @ : {get_sim_time(unit = 'ns')} ----------------")
+        self.dut._log.info("---------------- Reset Applied  ----------------")
         await ClockCycles(self.dut.clk,5)
-        cocotb.log.info(f"--------Reset Removed @ : {get_sim_time(unit = 'ns')} ----------------")
-        cocotb.log.info('-------------------------------------------------------------------------------')
+        self.dut._log.info("---------------- Reset Removed  ----------------")
+        self.dut._log.info("------------------------------------------------")
         self.dut.rst.value = 0
 
     async def recv_data(self):
         while True:
             temp = Transaction()
             temp = await self.queue.get()
-            cocotb.log.info(f"[DRV]: din: {temp.din} @ : {get_sim_time(unit = 'ns')}")
+            self.dut._log.info(f"[DRV]: din: {temp.din:02d}")
               
+            await self.dut.clk.falling_edge
             self.dut.din.value = temp.din
-            await self.dut.clk.rising_edge
-            self.dut.din.value = 0 # // Optional: Clear din after clock edge
             await self.dut.clk.rising_edge
 
 class Monitor():
@@ -72,15 +75,11 @@ class Monitor():
         while True:
             temp = Transaction()
             await self.dut.clk.rising_edge
-            temp.din = self.dut.din.value
-            # temp.dout = self.dut.dout.value # // Optional: Sample dout before clock edge
-            await self.dut.clk.rising_edge
-            temp.dout = self.dut.dout.value # Sample dout after clock edge
-            # temp.din = self.dut.din.value
-            
-            
+            await ReadOnly()
+            temp.din = int(self.dut.din.value)
+            temp.dout = int(self.dut.dout.value)
             await self.queue.put(temp)
-            cocotb.log.info(f"[MON] din: {temp.din} dout: {temp.dout} @ :{get_sim_time(unit = 'ns')}")
+            self.dut._log.info(f"[MON]: din: {temp.din:02d} dout: {temp.dout:02d}")
 
 class Scoreboard():
 
@@ -92,17 +91,20 @@ class Scoreboard():
     async def compare_data(self):
         while True:
             temp = await self.queue.get()           
-            cocotb.log.info(f"[SCO] din: {temp.din} dout: {temp.dout} @ : {get_sim_time(unit = 'ns')}")
+            cocotb.log.info(f"[SCO]: din: {temp.din:02d} dout: {temp.dout:02d}")
             if(temp.dout == temp.din):
-                cocotb.log.info("[SCO] : Test Passed")
+                cocotb.log.info("[SCO]: Test Passed")
             else:
-                cocotb.log.error("[SCO] : Test Failed")
+                cocotb.log.error("[SCO]: Test Failed")
             print('-------------------------')
                 
             self.event.set()
 
 @cocotb.test()
-async def test(dut):
+@cocotb.parametrize(
+    ("seq_random", [True, False]),
+    )
+async def test(dut, seq_random):
 
     random.seed(42)
     # Instantiate the classes
@@ -110,8 +112,11 @@ async def test(dut):
     queue_mon = Queue()
     event = Event()
     
+    # Number of transactions to be generated
+    n_events = 10
+    
     # Create objects
-    gen = Generator(queue_drv, event, 10)
+    gen = Generator(queue_drv, event, n_events)
     drv = Driver(queue_drv, dut)
     mon = Monitor(dut,queue_mon)
     sco = Scoreboard(queue_mon,event)
@@ -120,12 +125,22 @@ async def test(dut):
     cocotb.start_soon(Clock(dut.clk, 10, 'ns').start())
     await drv.reset_dut()
     
-    cocotb.start_soon(gen.gen_data())
-    cocotb.start_soon(drv.recv_data())
-    cocotb.start_soon(mon.sample_data())
+    driver_process = cocotb.start_soon(drv.recv_data())
+    monitor_process = cocotb.start_soon(mon.sample_data())
     cocotb.start_soon(sco.compare_data())
 
-    await Timer(200, 'ns')
+    await gen.gen_data(seq_random)  # Wait for the generator to finish generating transactions
+    while True:
+        if queue_mon.empty() and queue_drv.empty():
+            dut._log.info("All transactions processed.")
+            break
+        await ClockCycles(dut.clk, 1)
+    
+    dut._log.info("Stopping driver and monitor processes.")
+    driver_process.cancel()
+    monitor_process.cancel()
+
+    dut._log.info("Test completed.")
             
 
     
