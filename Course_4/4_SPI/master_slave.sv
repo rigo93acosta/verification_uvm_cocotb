6797c0d5716deeedaa79e48be285889fcc83a0fc
rigo93acosta <rigo93acosta@gmail.com>
2026-05-28 15:59:34 -0300

    fix: standardize formatting and spacing in master_slave module

--- Course_4/4_SPI/master_slave.sv
+++ Course_4/4_SPI/master_slave.sv
@@ -1,33 +1,33 @@
 module master_slave (
-    input          clk,
-    input          rst,
+    input         clk,
+    input         rst,
     input         newd,
-    input  [11:0]  din,
+    input  [11:0] din,
     output [11:0] dout,
     output        done
-  );
+);
 
-  wire        sclk;
-  wire        mosi;
-  wire         cs;
+  wire sclk;
+  wire mosi;
+  wire cs;
 
   SPI_MASTER spi_master (
-        .clk   (clk),
-        .rst   (rst),
-        .newd (newd),
-        .din   (din),
-        .sclk (sclk),
-        .mosi (mosi),
-        .cs     (cs)
-      );
+      .clk (clk),
+      .rst (rst),
+      .newd(newd),
+      .din (din),
+      .sclk(sclk),
+      .mosi(mosi),
+      .cs  (cs)
+  );
 
   SPI_SLAVE spi_slave_inst (
-              .sclk (sclk),
-              .cs     (cs),
-              .mosi (mosi),
-              .dout (dout),
-              .done (done)
-            );
+      .sclk(sclk),
+      .cs  (cs),
+      .mosi(mosi),
+      .dout(dout),
+      .done(done)
+  );
 
 
 endmodule
