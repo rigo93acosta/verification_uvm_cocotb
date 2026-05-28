b0bd26034de8910096ee799e4268dc341d2e6765
rigo93acosta <rigo93acosta@gmail.com>
2026-05-28 15:42:50 -0300

    fix: refactor class names and improve logging format in SPI testbench

--- Course_4/4_SPI/spi_master_tb.py
+++ Course_4/4_SPI/spi_master_tb.py
@@ -1,12 +1,11 @@
 import cocotb
-from cocotb.triggers import Timer, RisingEdge, ClockCycles, Event, FallingEdge
+from cocotb.triggers import ReadOnly, ClockCycles, Event
 from cocotb.clock import Clock
 from cocotb_coverage.crv import Randomized
 from cocotb.queue import Queue
-from cocotb.utils import get_sim_time
 
 
-class transaction(Randomized):
+class Transaction(Randomized):
     def __init__(self):
         Randomized.__init__(self)
         self.newd = 0
@@ -19,10 +18,10 @@ class transaction(Randomized):
         self.add_rand("din", list(range(4096)))
 
     def print_in(self, tag=""):
-        cocotb.log.info(f"{tag} : din = {self.din}")
+        cocotb.log.info(f"{tag}: din = {self.din:04d}")
 
 
-class generator:
+class Generator:
     def __init__(self, queue, event, count):
         self.queue = queue
         self.event = event
@@ -31,7 +30,7 @@ class generator:
 
     async def gen_data(self):
         for i in range(self.count):
-            t = transaction()
+            t = Transaction()
             t.randomize()
             t.print_in("[GEN]")
             await self.queue.put(t)
@@ -39,7 +38,7 @@ class generator:
             self.event.clear()
 
 
-class driver:
+class Driver:
     def __init__(self, dut, queue):
         self.dut = dut
         self.queue = queue
@@ -48,24 +47,23 @@ class driver:
         self.dut.rst.value = 1
         self.dut.din.value = 0
         self.dut.newd.value = 0
-        cocotb.log.info(f"Resetting DUT at time {get_sim_time('ns')} ns")
+        cocotb.log.info("============== Resetting DUT ==============")
         await ClockCycles(self.dut.clk, 5)
-        cocotb.log.info(f"Releasing DUT reset at time {get_sim_time('ns')} ns")
+        cocotb.log.info("============== Releasing DUT reset ==============")
         self.dut.rst.value = 0
 
-    async def recv_data(self):
+    async def send_data(self):
         while True:
-            temp = transaction()
+            temp = Transaction()
             temp = await self.queue.get()
             temp.print_in("[DRV]")
+            await self.dut.clk.falling_edge 
             self.dut.din.value = temp.din  # apply newdata
             self.dut.newd.value = 1
             await self.dut.sclk.rising_edge
-            self.dut.newd.value = 0
-            await self.dut.cs.rising_edge  # wait for completion of oper
 
 
-class monitor:
+class Monitor:
     def __init__(self, dut, queue):
         self.dut = dut
         self.queue = queue
@@ -77,21 +75,24 @@ class monitor:
             de shift register para reconstruir el dato recibido
             """
             dout = 0
-            temp = transaction()
+            temp = Transaction()
             await self.dut.cs.falling_edge  # wait for start of oper
             temp.din = self.dut.din.value
-            await self.dut.sclk.rising_edge  # sync to sclk
+            await self.dut.sclk.rising_edge # sync to sclk (mosi == 0)
             for i in range(12):
                 await self.dut.sclk.rising_edge
+                await ReadOnly()
                 dout = (dout << 1) | int(self.dut.mosi.value)
 
             await self.dut.cs.rising_edge  # wait for end of oper
             temp.dout = dout
             await self.queue.put(temp)
-            cocotb.log.info(f"[MON] din: {temp.din.to_unsigned()} : dout = {temp.dout}")
+            cocotb.log.info(
+                f"[MON]: din = {temp.din.to_unsigned():04d} : dout = {temp.dout:04d}"
+            )
 
 
-class scoreboard:
+class Scoreboard:
     def __init__(self, queue, event):
         self.queue = queue
         self.event = event
@@ -101,15 +102,16 @@ class scoreboard:
 
         while True:
             temp = await self.queue.get()
-            cocotb.log.info(f"[SCO] din: {temp.din.to_unsigned()} : dout = {temp.dout}")
 
             if temp.din == temp.dout:
-                cocotb.log.info(f"[SCO] Match: din = dout = {temp.din.to_unsigned()}")
+                cocotb.log.info(
+                    f"[SCO]: din = {temp.din.to_unsigned():04d} : dout = "
+                    f"{temp.dout:04d} => MATCH"
+                )
             else:
                 cocotb.log.error(
-                    f"[SCO] Mismatch: din = {temp.din.to_unsigned()} != dout = {
-                        temp.dout
-                    }"
+                    f"[SCO]: Mismatch: din = {temp.din.to_unsigned():04d} != "
+                    f"dout = {temp.dout:04d}"
                 )
 
             cocotb.log.info("--------------------------------------------------")
@@ -127,11 +129,12 @@ async def spi_master(dut):
     mon_queue = Queue()
     gen_event = Event()
 
+    n_data = 5
     # Instantiate components
-    gen = generator(drv_queue, gen_event, count=5)
-    drv = driver(dut, drv_queue)
-    mon = monitor(dut, mon_queue)
-    sco = scoreboard(mon_queue, gen_event)
+    gen = Generator(drv_queue, gen_event, count=n_data)
+    drv = Driver(dut, drv_queue)
+    mon = Monitor(dut, mon_queue)
+    sco = Scoreboard(mon_queue, gen_event)
 
     # Clock generation
     cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())  # 100 MHz clock
@@ -140,10 +143,19 @@ async def spi_master(dut):
     await drv.reset_dut()
 
     # Start component coroutines
-    cocotb.start_soon(gen.gen_data())
-    cocotb.start_soon(drv.recv_data())
-    cocotb.start_soon(mon.sample_data())
+    driver_process = cocotb.start_soon(drv.send_data())
+    monitor_process = cocotb.start_soon(mon.sample_data())
     cocotb.start_soon(sco.compare_data())
 
-    # Wait for all transactions to complete
-    await Timer(15000, "ns")
+    await gen.gen_data()  # Wait for generator to finish
+    cocotb.log.info("============== Generator finished ==============")
+    while True:
+        if drv_queue.empty() and mon_queue.empty():
+            cocotb.log.info("============== All queues empty ==============")
+            break
+        await ClockCycles(dut.clk, 1)  # Wait for queues to empty
+    
+    driver_process.cancel()
+    monitor_process.cancel()
+    await ClockCycles(dut.clk, 5)  # Allow processes to clean up
+    cocotb.log.info("============== Test completed ==============")
